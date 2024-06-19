7de82f9aee8c40c748575c106615db729b00ef07
Michael Eduardo <michael.eduardo@motivit.com>
2024-06-19 11:05:29 +0800

    Created counting algorithm

--- src/components/dashboard_components/Charts/PlatformChart.vue
+++ src/components/dashboard_components/Charts/PlatformChart.vue
@@ -3,22 +3,29 @@ import Chart from 'chart.js/auto';
 import { ref, watch, onMounted } from 'vue'
 import axios from 'axios'
 
-var chart_platform_label = ref([])
+var chart_label = ref([])
+var chart_data = ref([])
 var chart_platform_data = ref([])
 const platform_chart_id = ref('platform_chart')
 
 const fetch_platforms_data = () => {
     axios.get('http://127.0.0.1:3000/analysis/platform').then(res => {
         for (let i = 0; i < res.data.platform_list.length; i++) {
-            chart_platform_label.value.push(res.data.platform_list[i].platform_name)
+            chart_platform_data.value.push({ label: res.data.platform_list[i].platform_name, count: 0 })
         }
         for (let i = 0; i < res.data.platform_analysis.length; i++) {
-            console.log(res.data.platform_analysis[i])
+            for (let x = 0; x < chart_platform_data.value.length; x++) {
+                for (let y = 0; y < res.data.platform_analysis[i].platforms_data.length; y++) {
+                    if(chart_platform_data.value[x].label === res.data.platform_analysis[i].platforms_data[y]) {
+                        chart_platform_data.value[x].count += 1
+                    }
+                }
+            }
+        }
 
-            // const recurringValue = chart_platform_label.filter((value, index, self) => {
-        //         return self.indexOf(value) !== index;
-        //     });
-        // console.log(recurringValue)
+        for (let i = 0; i < chart_platform_data.value.length; i++) {
+            chart_label.value.push(chart_platform_data.value[i].label)
+            chart_data.value.push(chart_platform_data.value[i].count)
         }
     })
 }
@@ -27,12 +34,12 @@ onMounted(() => {
     fetch_platforms_data()
 })
 
-watch(chart_platform_label.value, () => {
+watch(chart_platform_data.value, () => {
     const data = ref({
-        labels: chart_platform_label.value,
+        labels: chart_label.value,
         datasets: [{
             label: 'Platform Data',
-            data: [1,2,3,4,5]
+            data: chart_data.value
         }]
     })
     const config = ref({
@@ -40,12 +47,8 @@ watch(chart_platform_label.value, () => {
         data: data.value
     })
 
-    new Chart(chart_id.value, config.value)
+    new Chart(platform_chart_id.value, config.value)
 })
-
-
-
-
 </script>
 
 <template>
