780d554208a9954fec3f7167856250de2382643e
Michael Eduardo <michael.eduardo@motivit.com>
2024-06-24 09:20:27 +0800

    Changing from charts to tables

--- src/components/dashboard_components/Charts/HiringCharts.vue
+++ src/components/dashboard_components/Charts/HiringCharts.vue
@@ -1,23 +1,12 @@
 <script setup>
 import axios from 'axios'
 import { ref, onMounted, watch } from 'vue'
-import Chart from 'chart.js/auto';
 
-const hiring_progress = ref("hiring_chart")
-const hiring_progress_data = ref([])
-const hiring_chart_label = ref([])
-const hiring_chart_data = ref([])
+const hiring_slots_data = ref([])
 
 const get_job_lists = () => {
     axios.get('http://127.0.0.1:3000/jobs').then(res => {
-        hiring_progress_data.value = res.data.job_positions
-        console.log(hiring_progress_data.value)
-        for (var i = 0; i < hiring_progress_data.value.length; i++) {
-            if (hiring_progress_data.value[i].available_slot > 0) {
-                hiring_chart_label.value.push(hiring_progress_data.value[i].position_name)
-                hiring_chart_data.value.push(hiring_progress_data.value[i].available_slot)
-            }
-        }
+        hiring_slots_data.value = res.data.job_positions
     })
 }
 
@@ -25,38 +14,19 @@ onMounted(() => {
     get_job_lists()
 })
 
-watch(hiring_progress_data, () => {
-    const data = ref({
-        labels: hiring_chart_label.value,
-        datasets: [{
-            label: "Available Positions",
-            data: hiring_chart_data.value,
-            backgroundColor: '#60a5fa',
-            borderRadius: 10
-        }]
-    })
 
-    const config = {
-        type: 'bar',
-        data: data.value,
-        options: {
-            maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
-        }
-    }
-
-    new Chart(hiring_progress.value, config)
-
-})
 </script>
 
 <template>
-    <div class="h-[45dvh] mt-4">
-        <canvas :id="hiring_progress">
-        </canvas>
+    <div class="h-[45dvh] mt-2 grid grid-cols-2 overflow-y-auto px-4">
+        <div v-for="hiring in hiring_slots_data" class="flex items-center my-2 w-[47dvh] h-[16dvh] shadow-lg rounded-lg p-2">
+            <div class="flex items-center justify-center bg-blue-500 h-[11dvh] w-[10dvh] mx-2 rounded-lg">
+                <h1 class="text-3xl text-white font-bold ">{{ hiring.available_slot }}</h1>
+            </div>
+            <span class="flex flex-col">
+                <h1 class="font-bold text-blue-600">{{ hiring.position_name }}</h1>
+                <h1 class="text-sm text-gray-500">{{ hiring.department_name }}</h1>
+            </span>
+        </div>
     </div>
 </template>
